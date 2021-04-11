b4c0ce30fc93904cb9c3761989a66c120f90e536
Jonathan Sedrez <jonathansedrez@gmail.com>
2021-04-11 10:54:53 -0300

    add base structure; add scss

---
+++ src/containers/unregister/unregister.container.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Unregister:React.FC = () => {
+return (
+  <div>
+    <p>unregister</p>
+  </div>
+)
+}
---
+++ src/layout/variables.ts
@@ -0,0 +1,3 @@
+export enum Color {
+  grey = '#c9c9c9'
+}
