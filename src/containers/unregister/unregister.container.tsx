74e8a1a3bd99086cfe80a32e491e01fa90d1c812
Jonathan Sedrez <jonathansedrez@gmail.com>
2021-04-13 23:12:35 -0300

    fix styles

--- src/containers/unregister/unregister.container.tsx
+++ src/containers/unregister/unregister.container.tsx
@@ -19,53 +19,58 @@ export const Unregister: React.FC = () => {
 
   return (
     <Card>
-      <div className="form">
-        <div className="form__disclaimer">
+      <div className="unregister">
+        <div className="unregister__disclaimer">
           <CompanyLogoIcon />
-          <h1 className="form__title">Nós sentiremos sua falta</h1>
-          <div className="form__text">
-            <p>
+          <h1 className="unregister__title">Nós sentiremos sua falta</h1>
+          <div className="unregister__text-wrapper">
+            <p className="unregister__text">
               Lamentamos ver você indo embora. <br /> Para que possamos
               <b> melhorar nossa comunicação</b>, gostaríamos de
               <b> saber os motivos</b> que o levaram a tomar essa decisão.
             </p>
-            <p>
+            <p className="unregister__text">
               <b>O questionário é opcional.</b>
             </p>
           </div>
         </div>
-        <form
-          onSubmit={(event) => event.preventDefault()}
-          className="form__inputs"
-        >
-          <Radio onChange={setSelectedJustification} name="justification">
-            {justifications.map(({ id, value, hasDescription }, index) => {
-              return (
-                <Radio.Option key={id} id={id}>
-                  <Fragment>
-                    <p>{value}</p>
-                    {hasDescription && (
-                      <Input
-                        placeholder="Informe o motivo"
-                        value={justifications[index].description || ''}
-                        onChange={(event) => {
-                          let just = [...justifications];
-                          console.log(just[index]);
-                          just[index] = {
-                            ...just[index],
-                            description: event.target.value,
-                          };
-                          setJustifications(just);
-                        }}
-                      />
-                    )}
-                  </Fragment>
-                </Radio.Option>
-              );
-            })}
-          </Radio>
-          <Button onClick={handleSubmit}>Descadastrar</Button>
-        </form>
+        <div className="unregister__inputs">
+          <h2 className="unregister__subtitle">
+            Por que você quer se descadastrar?
+          </h2>
+          <form onSubmit={(event) => event.preventDefault()}>
+            <Radio onChange={setSelectedJustification} name="justification">
+              {justifications.map(({ id, value, hasDescription }, index) => {
+                return (
+                  <Radio.Option key={id} id={id}>
+                    <Fragment>
+                      <p className="unregister__radio-text">{value}</p>
+                      {hasDescription && (
+                        <Input
+                          placeholder="Informe o motivo"
+                          value={justifications[index].description || ''}
+                          onChange={(event) => {
+                            let just = [...justifications];
+                            just[index] = {
+                              ...just[index],
+                              description: event.target.value,
+                            };
+                            setJustifications(just);
+                          }}
+                        />
+                      )}
+                    </Fragment>
+                  </Radio.Option>
+                );
+              })}
+            </Radio>
+            <p className="unregister__text">
+              O descadastramento será efetuado para o e-mail: <br />
+              <i>nome.sobrenome@pmweb.com.br</i>
+            </p>
+            <Button onClick={handleSubmit}>Descadastrar</Button>
+          </form>
+        </div>
       </div>
     </Card>
   );
